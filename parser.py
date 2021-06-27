c5a2e7ec9fc69649d3d13bf3db68ef869d77ddba
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-27 12:50:09 -0400

    Update parser.py (#3)

--- parser.py
+++ parser.py
@@ -1,19 +1,25 @@
 from .lexer import generator
 
+from typing import List
 from rply import ParserGenerator, Token
+
 
 parser = ParserGenerator([l.name for l in generator.rules])
 
+
 class rooBulli(SyntaxError):
     pass
-
+  
+  
 @parser.production('string : STRING')
-def string(p: list) -> str:
+def string(p: List[...]) -> str:
     return p[0].getstr().strip("'").strip('"')
 
-@parser.production("expr: rooPog string")
-def rooPog(p: list) -> None:
+
+@parser.production("expr : rooPog string")
+def rooPog(p: List[...]) -> None:
     ...
+    
 
 @parser.error
 def rooBulli_error_handler(token):
