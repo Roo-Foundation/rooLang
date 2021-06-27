cfe188e8ed2c02c9a49a5f3cdf2b36c0bddcad9b
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-27 12:42:44 -0400

    Make lexer follow PEP 8

--- lexer.py
+++ lexer.py
@@ -1,10 +1,11 @@
 from rply import LexerGenerator
 
+
 generator = LexerGenerator()
 
-generator.ignore(r'\s+')  # ignore all whitespace
-generator.ignore(r"rooWhisper.*") # Ignore single line comment
+generator.ignore(r"\s+")  # ignore all whitespace
+generator.ignore(r"rooWhisper.*")  # Ignore single line comment
 
 generator.add("STRING", r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')''')
 
-generator.add("rooPog", r"rooPog")
+generator.add("rooPog", "rooPog")
