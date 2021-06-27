ebaef7102087b9b2db4dac75b384becda47e24c8
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-27 12:46:28 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,13 +1,18 @@
 from .lexer import generator
 
+from typing import List
 from rply import ParserGenerator, Token
+
 
 parser = ParserGenerator([l.name for l in generator.rules])
 
+
 @parser.production('string : STRING')
-def string(p: list) -> str:
+def string(p: List[...]) -> str:
     return p[0].getstr().strip("'").strip('"')
 
-@parser.production("expr: rooPog string")
-def rooPog(p: list) -> None:
+
+@parser.production("expr : rooPog string")
+def rooPog(p: List[...]) -> None:
+    ...
     
