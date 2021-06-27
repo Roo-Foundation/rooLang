54949624209e5cd5b272e293715223b3ce92ef97
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-27 09:46:22 -0700

    error handle :O

--- parser.py
+++ parser.py
@@ -4,10 +4,17 @@ from rply import ParserGenerator, Token
 
 parser = ParserGenerator([l.name for l in generator.rules])
 
+class rooBulli(SyntaxError):
+    pass
+
 @parser.production('string : STRING')
 def string(p: list) -> str:
     return p[0].getstr().strip("'").strip('"')
 
 @parser.production("expr: rooPog string")
 def rooPog(p: list) -> None:
-    
+    ...
+
+@parser.error
+def rooBulli_error_handler(token):
+    raise rooBulli(f"rooBulli: Unexpected {token.gettokentype()} in {token.getsourcepos()}\nvalue : '{token.getstr()}'")
